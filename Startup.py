895ff0e7c0ca44ef960d06e94689960a9d3dd300
rgoogoonani <68652911+rgoogoonani@users.noreply.github.com>
2023-03-14 22:38:31 +0330

    Update Startup.py

--- Startup.py
+++ Startup.py
@@ -21,10 +21,10 @@ with open("/NginxBackup/config.txt","w") as f:
     f.writelines(f"{chid}\n{Name}\n{FileAddres}\n{botToken}")
 
 
-with open("/etc/systemd/system/x-uiAutoBackup.service","w") as f:
-    f.writelines("[Unit]\nDescription=x-ui Auto Backup\n\n[Service]\nExecStart=/usr/bin/python3 /NginxBackup/AutoBackup.py\n\n[Install]\nWantedBy=multi-user.target")
-#f.writelines("[Unit]\nDescription=x-ui Auto Backup\n\n[Service]\nExecStart=screen /usr/bin/python3 "+str(os.path.abspath(__file__))+"\n\n[Install]\nWantedBy=multi-user.target")
+with open("/etc/systemd/system/NginxAutoBackup.service","w") as f:
+    f.writelines("[Unit]\nDescription=Nginx Auto Backup\n\n[Service]\nExecStart=/usr/bin/python3 /NginxBackup/AutoBackup.py\n\n[Install]\nWantedBy=multi-user.target")
+#f.writelines("[Unit]\nDescription=Nginx Auto Backup\n\n[Service]\nExecStart=screen /usr/bin/python3 "+str(os.path.abspath(__file__))+"\n\n[Install]\nWantedBy=multi-user.target")
 
 os.system("sudo systemctl daemon-reload")
-os.system("sudo systemctl start x-uiAutoBackup")
-os.system("sudo systemctl enable x-uiAutoBackup")
+os.system("sudo systemctl start NginxAutoBackup")
+os.system("sudo systemctl enable NginxAutoBackup")
